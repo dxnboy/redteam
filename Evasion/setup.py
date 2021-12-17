6cf71832b5ac14910e32150d3210195cb82d1a78
dxnboy <47841843+dxnboy@users.noreply.github.com>
2021-12-17 13:53:40 +0700

    Create setup.py

---
+++ Evasion/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+import py2exe
+setup(
+name = 'Meter',
+description = 'Python-based App',
+version = '1.0',
+console=['mrtp.py'],
+options = {'py2exe': {'bundle_files': 1,'packages':'ctypes','includes': 'base64,sys,socket,struct,time,code,platform,getpass,shutil',}},
+zipfile = None,
+)
